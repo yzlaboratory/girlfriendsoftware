279fb9f829932ab2fc8eb7ad2fbed20c4eadfa7b
yz <x@yzlaboratory.dev>
2024-09-11 15:28:35 +0200

    zero draft

--- apps/itinerary/src/app/app.component.ts
+++ apps/itinerary/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from "@angular/core";
 import { RouterModule } from "@angular/router";
-import { NxWelcomeComponent } from "./nx-welcome.component";
+import { HomeComponent } from "./home/home.component";
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [RouterModule, HomeComponent],
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.css",
